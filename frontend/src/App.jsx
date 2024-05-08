f37028c2c08af86b26a7955b83a730d43f70927f
ChienPhamUET <22022634@vnu.edu.vn>
2024-05-08 16:05:42 +0700

    Bổ sung React cho Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Routes>
+        <Route path='' element={<></>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
