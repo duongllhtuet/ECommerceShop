f3053187230792200f250464e6fdf97d8627fc49
duongllhtuet <leduong.uet.ai@gmail.com>
2024-05-15 20:25:31 +0700

    HLD modify backend

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -28,66 +28,69 @@ const Home = () => {
 
   return (
     <div className="App__Container">
-        <div className="grid wide">
-            <div className="row sm--gutter App_Content">
-                <div className="col l-2 m-0 c-0">
-                    <nav className="Category">
-                    <h3 className="Category__Heading">
-                        Danh mục
-                    </h3>
-
-                    <ul className="Category--List">
-                        <li className="Category--Item">
-                            <div onClick={()=>setSex(prev=>"All")} className="Category--Item__Link">Tất cả</div>
-                        </li>
-
-                        <li className="Category--Item">
-                            <div onClick={()=>setSex(prev=>"Men")} className="Category--Item__Link">Thời trang nam</div>
-                        </li>
-
-                        <li className="Category--Item">
-                            <div onClick={()=>setSex(prev=>"Women")} className="Category--Item__Link">Thời trang nữ</div>
-                        </li>
-                    </ul>
-                    </nav>
-                </div>
-
-                <div className="col l-10 m-12 c-12">
-                    <div className="Home--Products">
-                        <div className="row sm--gutter">
-                            <div className="col l-2-4 m-4 c-6">
-                                {/* <!-- Product Item --> */}
-                                {data.map((item, index) => {
-                                    if(sex==="All" || sex===item.sex){
-                                        return (
-                                            <ProductItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.picture} selling={item.selling} />
-                                        )
-                                    }
-                                })}
-                            </div>
+      <div className="grid wide">
+        <div className="row sm--gutter App_Content">
+          <div className="col l-2 m-0 c-0">
+            <nav className="Category">
+              <h3 className="Category__Heading">Danh mục</h3>
+              <ul className="Category--List">
+                <li className="Category--Item">
+                  <div
+                    onClick={() => setSex("All")}
+                    className="Category--Item__Link"
+                  >
+                    Tất cả
+                  </div>
+                </li>
+                <li className="Category--Item">
+                  <div
+                    onClick={() => setSex("Men")}
+                    className="Category--Item__Link"
+                  >
+                    Thời trang nam
+                  </div>
+                </li>
+                <li className="Category--Item">
+                  <div
+                    onClick={() => setSex("Women")}
+                    className="Category--Item__Link"
+                  >
+                    Thời trang nữ
+                  </div>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="col l-10 m-12 c-12">
+            <div className="Home--Products">
+              <div className="row sm--gutter">
+                {data.map((item, index) => {
+                  if ((sex === "All" || sex === item.sex)) {
+                    const isNewRow = index % 5 === 0 && index !== 0;
+                    return (
+                      <React.Fragment key={index}>
+                        {isNewRow && <div className="row sm--gutter"></div>}
+                        <div className="col l-2-4 m-4 c-6">
+                          <ProductItem
+                            id={item._id}
+                            name={item.name}
+                            description={item.description}
+                            price={item.price}
+                            image={item.picture}
+                            selling={item.selling}
+                          />
                         </div>
-                    </div>
-
-                {/* <ul className="Pagnination Home--Product__Pagination">
-                <li className="Pagnination--Item">
-                    <a href="" className="Pagnination--Item__Link">
-                    <i className="Pagnination--Item__Icon fas fa-angle-left"></i>
-                    </a>
-                </li>
-
-                <li className="Pagnination--Item Pagnination--Item--Active">
-                    <a href="" className="Pagnination--Item__Link">1</a>
-                </li>
-
-                <li className="Pagnination--Item">
-                    <a href="" className="Pagnination--Item__Link">
-                    <i className="Pagnination--Item__Icon fas fa-angle-right"></i>
-                    </a>
-                </li>
-                </ul> */}
-                </div>
+                      </React.Fragment>
+                    );
+                  } else {
+                    return null;
+                  }
+                })}
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
   );
 };
