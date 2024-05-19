6ddf59484f060ff59855408d3cf6ddca0a3f1cd0
ChienPhamUET <22022634@vnu.edu.vn>
2024-05-19 20:30:59 +0700

    lỗi thanh toán

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order from frontend
 const placeOrder = async (req, res) => {
     
-    const frontend_url = "https://testdeploy-1-h7zb.onrender.com"
+    const frontend_url = "https://ecommerceshop-test.onrender.com"
 
     try {
         const newOrder = new orderModel({
